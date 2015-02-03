adc49bc8490d8ff2b03a2e52d4eb14f9b82cbbb3
viksachdeva <vsachdeva@gmail.com>
2015-02-03 15:23:05 +0530

    Update Count.hs

--- Count.hs
+++ Count.hs
@@ -15,4 +15,28 @@ apply Div x y = x `div` y
 data Expr = Val Int | App Op Expr Expr
 values :: Expr -> [Int]
 values (Val n) = [n]
+values (App  _ l r) = values l ++ values r
+
+eval :: Expr -> [Int]
+eval (Val n ) = [n|n>0]
+eval (App o l r) = [ apply o x y | x <- eval l, y <- eval r, valid o x y ]
+
+subs :: [a] -> [[a]]
+subs[] = [[]]
+subs (x:xs) = yss ++ map (x:) yss
+              where yss = subs xs
+
+interleave :: a ->[a]->[[a]]
+interleave x[] = [[x]]
+interleave x(y:ys) = (x:y:ys):map(y:)(interleave x ys)
+
+perms :: [a] -> [[a]]
+perms [] = [[]]
+perms (x:xs) = concat(map(interleave x)(perms xs))
+
+choices :: [a] -> [[a]]
+choices xs = concat(map perms(subs xs))
+
+solution :: Expr->[Int]->Int->Bool
+solution e ns n = elem (values e) (choices ns)  eval e == [n]
 
